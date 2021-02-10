8e5a940f67cfc1a20545de34a98f1264893fc1ce
Jenny Aguilar <contact@jaguilarweb.com>
2021-02-10 18:05:05 -0300

    refactor: Add CORS headers app.py

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import os
 from flask import Flask
 from models import setup_db
+from flask_cors import CORS
 
 def create_app(test_config=None):
 
     app = Flask(__name__)
     setup_db(app)
     CORS(app)
+    
+  # CORS Headers 
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        return response
 
     @app.route('/')
     def get_greeting():
